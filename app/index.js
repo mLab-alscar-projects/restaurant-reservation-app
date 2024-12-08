9c9bbfcb39ae2ec1ee9a8cc25ef822b555882920
Nhlakanipho <nhlakaniphoradebe337@gmail.com>
2024-12-08 22:06:50 +0100

    Navigation: app folder with _layout and index for page navigation

--- app/index.js
+++ app/index.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function index() {
+export default function RegisterScreen() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello I am online!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,6 +18,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-
-
-
