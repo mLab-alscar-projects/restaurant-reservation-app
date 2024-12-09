7e83577b9c6fca5b8babf1c8352ed81019577eca
Oscar Poco <123397536+Oscarpoco@users.noreply.github.com>
2024-12-09 09:18:13 +0200

    I have implemented the splash screen  and login screen

--- app/index.js
+++ app/index.js
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import { View, ActivityIndicator} from 'react-native';
+import { View, ActivityIndicator, StyleSheet} from 'react-native';
 import { useRouter } from 'expo-router';
 import { useState, useEffect } from 'react';
-import SplashScreenChild from '../Components/splash';
+import SplashScreenChild from '../Components/SplashScreen';
 
 export default function Index() {
   const router = useRouter();
@@ -12,7 +12,7 @@ export default function Index() {
   useEffect(() => {
     const splashTimeout = setTimeout(() => {
       setShowSplash(false); 
-      router.push("/homePage"); 
+      router.push("/LoginScreen"); 
     }, 3000); 
 
     return () => clearTimeout(splashTimeout); 
@@ -20,10 +20,38 @@ export default function Index() {
 
 // Display splash screen
   return (
-    <View style={{ flex: 1 }}>
-      <StatusBar style="auto" />
-      {showSplash && <SplashScreenChild />}
+    <View style={styles.splashContainer}>
+
+      <View style={styles.splashLogo}>
+        <StatusBar style="auto" />
+        {showSplash && <SplashScreenChild />}
+      </View>
+
       <ActivityIndicator size="large" color="#0000ff" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+
+  // SPLASH
+  splashContainer: 
+  {
+      flex: 1,
+      backgroundColor: '#fff',
+      justifyContent: 'center',
+      alignItems: 'center',
+      gap: 40
+  },
+
+
+  splashLogo: 
+  {
+      width: '100%',
+      height: 400,
+      padding: 50
+  },
+
+  // ENDS
+
+});
